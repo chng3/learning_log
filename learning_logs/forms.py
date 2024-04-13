190934fcaa08d5b73ccd8b7094136fe502c51a90
Zhuang <chngdolphin@gmail.com>
2024-04-14 04:16:56 +0800

    完成19.1.2

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
-        
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text':''}
+        # 定制了字段‘text’的输入小部件，将文本宽度设置为80列，而不是默认的40列
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+    
+
